5f5681c240b26b93222a59a94c3f426568aef355
heart47315-stack <heart47315@gmail.com>
2026-08-28 09:32:58 +0700

    1

--- my-react-jsx/src/components/Button.jsx
+++ my-react-jsx/src/components/Button.jsx
@@ -9,5 +9,13 @@ function Button({ text }) {
     </button>
   );
 }
+<body>
+  background-color: #555faa;
+  background-image: linear-gradient(315deg, #555faa 0%, #f5f7fa 74%);
+  background-repeat: no-repeat; 
+  background-attachment: fixed;
+  background-size: cover;
+  scroll-behavior: smooth;
+</body>
 
 export default Button;
--- my-react-jsx/src/components/Header.jsx
+++ my-react-jsx/src/components/Header.jsx
@@ -7,3 +7,11 @@ function Header() {
 }
 
 export default Header;
+<body>
+  background-color: #555faa;
+  background-image: linear-gradient(315deg, #555faa 0%, #f5f7fa 74%);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  scroll-behavior: smooth;
+</body>
